43de4de46e37759aa7d2ea2ce977f9eaa2cfc578
mad0 <madowaty@gmail.com>
2017-03-01 14:24:18 +0100

    Added: Engine class

--- SurvivalConsole/main.cpp
+++ SurvivalConsole/main.cpp
@@ -5,6 +5,26 @@
 #include <map>
 #include <memory>
 
+class Engine {
+private:
+	sf::RenderWindow &okno;
+public:
+	void input() {
+		sf::Event event;
+		while (okno.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				okno.close();
+			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
+				okno.close();
+		}
+	};
+
+	Engine(sf::RenderWindow &okno) :okno(okno) {}
+	~Engine() {}
+
+};
+
 class Tekst {
 private:
 	sf::Text text;
@@ -14,6 +34,7 @@ private:
 	sf::Vector2f poz;
 public:
 	Tekst(const std::string& data, const std::string& czcionka, sf::Vector2f poz) : data(data), czcionka(czcionka), poz(poz) {
+		std::cout << "Tworze obiekt klasy Tekst...\n";
 		font.loadFromFile(czcionka);
 		text.setFont(font);
 		text.setString(data);
@@ -27,7 +48,7 @@ public:
 	};
 
 	~Tekst() {
-
+		std::cout << "Usuwam obiekt klasy Tekst...\n";
 	};
 };
 
@@ -38,30 +59,26 @@ int main() {
 	window.setFramerateLimit(30);
 	window.setVerticalSyncEnabled(true);
 
-	sf::Vector2f pos = sf::Vector2f(50, 50);
+	sf::Vector2f pos = sf::Vector2f(50, 100);
 	std::string tekst1 = "Obudzilem sie w jakiejs ciemnej piwnicy, z przodu widze korytarz\nCo robic?";
+	sf::Vector2f pos2 = sf::Vector2f(50, 200);
+	std::string tekst2 = "Chyba pojde do przodu, moze cos znajde...";
 
 	
 	std::map<std::string, std::unique_ptr<Tekst>> teksty;
 	teksty.insert(std::make_pair("t1", std::unique_ptr<Tekst>(new Tekst(tekst1, "FBI.ttf", pos))));
-
+	teksty.insert(std::make_pair("t2", std::unique_ptr<Tekst>(new Tekst(tekst2, "FBI.ttf", pos2))));
+	Engine engine(window);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
-				window.close();
-		}
-
+		engine.input();
 		window.clear();
 		teksty["t1"]->draw(window);
+		teksty["t2"]->draw(window);
 		window.display();
 	}
 
-
+	std::cout << "EXIT...\n";
 	return 0;
 };
